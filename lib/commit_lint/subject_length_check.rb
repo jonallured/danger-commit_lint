0ca51a2f8cfbedb8ffe0d5987361bbe87045b59b
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2017-08-10 16:46:45 -0300

    Fix line length rubocop warnings

--- lib/commit_lint/subject_length_check.rb
+++ lib/commit_lint/subject_length_check.rb
@@ -2,7 +2,8 @@ module Danger
   class DangerCommitLint < Plugin
     class SubjectLengthCheck < CommitCheck # :nodoc:
       MESSAGE = 'Please limit commit subject line to 50 characters.'.freeze
-      GIT_GENERATED_SUBJECT = /^Merge ?(pull request #\d+ from\ |branch \'.+\' into\ )/
+      GIT_GENERATED_SUBJECT = /^Merge (pull request #\d+ from\ |\
+                                       branch \'.+\' into\ )/
 
       attr_reader :subject
 
